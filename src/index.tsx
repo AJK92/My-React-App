9fb12a4bf220e2b65971b59c37a8d8e028b133b7
unknown <lookabi@gmail.com>
2018-12-27 08:44:33 +0530

    code clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import {Provider} from 'react-redux'
-// import {createStore} from 'redux'
-// import getGalleryListReducer from './Reducers/gallery-reducers'
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-
-// const store = createStore(getGalleryListReducer)
 
 ReactDOM.render(
   <App />,
